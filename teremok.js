72b7030726b693474476e6c55cea9954b77e44d0
oduvanio <oduvanio@mail.ru>
2017-08-18 23:55:12 +0400

    Update teremok.js

--- teremok.js
+++ teremok.js
@@ -22,15 +22,28 @@ window.Teremok = {
 						for (var i = 0, l = images.length; i < l; i++) {
 							$(this).append('<div class="image"></div>');
 						}
+						if (images[i].btnhref !== null) {
+							$(this).append('<a class="btn ' + images[i].btnhref.styleBtn + ' buttonTeremok" href="' + images[i].btnhref.href + '">' + images[i].btnhref.title + '</a>');	
+						} else {
+							$(this).append('<a class="buttonTeremok" href="">""</a>');
+						}
 					});
 					var height = div.css('height');
 					
 					var image = div.find('.image');
+					var button = div.find('.buttonTeremok');
 					image.css({opacity: 0.0, 'height':height, 'background-image':'url(/vendor/infrajs/imager/?w=2500&src=' + images[0] + ')'});
+					button.css({'display': 'none'});
 					image.eq(0).animate({opacity: 1.0}, 3000).toggleClass('scale');
+					if (image.eq(0).next('.buttonTeremok')[0].innerHTML !== "\"\"") {
+						image.eq(0).next('.buttonTeremok').css({'display': 'block'}).animate({opacity: 1.0}, 2000);
+					}
 					
 					var i = 1;
 					var timer = setTimeout(function tick(){
+						var div = $('.teremok');
+						var button = div.find('.buttonTeremok');
+						button.css({'display': 'none', opacity: 0.0});
 						var height = div.css('height');
 						
 						if (i >= images.length) {
@@ -38,8 +51,14 @@ window.Teremok = {
 						}
 						if (i == 0) {
 							image.last().animate({opacity: 0.0}, 1000);
+							if (image.eq(i).next('.buttonTeremok')[0].innerHTML !== "\"\"") {
+								image.eq(i).next('.buttonTeremok').css({'display': 'block'}).animate({opacity: 1.0}, 2000);
+							}
 						} else {
 							image.eq(i-1).animate({opacity: 0.0}, 1000);
+							if (image.eq(i).next('.buttonTeremok')[0].innerHTML !== "\"\"") {
+								image.eq(i).next('.buttonTeremok').css({'display': 'block'}).animate({opacity: 1.0}, 2000);
+							}
 						}
 						image.eq(i).css({'height':height, opacity: 0.0, 'background-image':'url(/vendor/infrajs/imager/?w=2500&src=' + images[i++] +')'}).toggleClass('scale')
 						.animate({opacity: 1.0}, 3000);
